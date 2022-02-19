fc8a7846da918732b645dcd7760310d02fac8c27
Ксения <94642483+ksssharova@users.noreply.github.com>
2022-02-19 16:35:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,10 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    int fact(int n) {
-        if (n==1)
-            return 1
-        else
-            return n * fact(n-1);
-    }
-    std::cout << fact(5)
+    while (a > 0 && b > 0)
+        if (a > b)
+            a %= b;
+        else 
+            b %= a;
+    return a + b;
 }
